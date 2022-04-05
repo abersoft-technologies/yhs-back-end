bb7d716c8374350b72120b458b45bf8d2483f412
Jon Sundelöf <70135228+jon-sundelof@users.noreply.github.com>
2022-04-05 09:59:21 +0200

    YS-45 Created GET list API for contacts

--- app/routes/contact.route.js
+++ app/routes/contact.route.js
@@ -4,5 +4,6 @@ const { contactController } = require('../controllers');
 const router = express.Router();
 
 router.post('/', contactController.addContact);
+router.get('/', contactController.getContactList);
 
 module.exports = router;
